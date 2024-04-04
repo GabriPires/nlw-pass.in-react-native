8a29b1b7f9437fab53b76e4e644f202ec49dd584
Gabriel Pires <gabpiresdev@gmail.com>
2024-04-04 12:06:23 -0300

    feat: animate credential with moti

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,9 +1,11 @@
 import { Feather } from '@expo/vector-icons'
+import { MotiView } from 'moti'
 import {
   Image,
   ImageBackground,
   Text,
   TouchableOpacity,
+  useWindowDimensions,
   View,
 } from 'react-native'
 
@@ -22,8 +24,34 @@ export function Credential({
   onChangeAvatar,
   onShowQRCode,
 }: CredentialProps) {
+  const { height } = useWindowDimensions()
+
   return (
-    <View className="w-full self-stretch items-center">
+    <MotiView
+      className="w-full self-stretch items-center"
+      from={{
+        opacity: 0,
+        translateY: -height,
+        rotateZ: '50deg',
+        rotateY: '30deg',
+        rotateX: '30deg',
+      }}
+      animate={{
+        opacity: 1,
+        translateY: 0,
+        rotateZ: '0deg',
+        rotateY: '0deg',
+        rotateX: '0deg',
+      }}
+      transition={{
+        type: 'spring',
+        damping: 20,
+        rotateZ: {
+          damping: 15,
+          mass: 3,
+        },
+      }}
+    >
       <Image
         source={require('@/assets/ticket/band.png')}
         alt=""
@@ -79,6 +107,6 @@ export function Credential({
           </Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </MotiView>
   )
 }
