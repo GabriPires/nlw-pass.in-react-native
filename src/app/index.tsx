a6f1a9da456dd355544330828be3f413618eb5d6
Gabriel Pires <gabpiresdev@gmail.com>
2024-04-04 08:59:03 -0300

    feat: finish input component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,12 @@
-import { Image, Text, View } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { Image, View } from 'react-native'
+
+import { Input } from '@/components/input'
+import { colors } from '@/styles/colors'
 
 export default function Home() {
   return (
-    <View className="bg-green-500 flex-1 items-center justify-center">
+    <View className="bg-green-500 flex-1 items-center justify-center p-8">
       <Image
         source={require('@/assets/logo.png')}
         className="h-16"
@@ -10,7 +14,16 @@ export default function Home() {
         alt="Logo"
       />
 
-      <View></View>
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="Código do ingresso" />
+        </Input>
+      </View>
     </View>
   )
 }
