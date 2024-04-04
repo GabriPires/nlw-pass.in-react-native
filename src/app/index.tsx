f5d2f9af37a6b82cc13e0a11217693e4169173eb
Gabriel Pires <gabpiresdev@gmail.com>
2024-04-04 08:45:55 -0300

    feat: initial input component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 export default function Home() {
   return (
     <View className="bg-green-500 flex-1 items-center justify-center">
-      <Text className="text-white font-bold">Hello world</Text>
+      <Image
+        source={require('@/assets/logo.png')}
+        className="h-16"
+        resizeMode="contain"
+        alt="Logo"
+      />
+
+      <View></View>
     </View>
   )
 }
---
+++ src/components/input.tsx
@@ -0,0 +1,11 @@
+import { TextInput, View } from 'react-native'
+
+interface InputProps {}
+
+function Input(props: InputProps) {
+  return <View></View>
+}
+
+function Field() {
+  return <TextInput />
+}
